58cc22e60bfab55ddf13ffc694c679727a47f471
Vaughn Ohlerking <vaughnohlerking@gmail.com>
2022-11-30 15:54:40 -0600

    knn attempt

---
+++ src/KNN/knn_classifier.py
@@ -0,0 +1,9 @@
+import sklearn as sk
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
